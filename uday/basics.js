b61edb61ebede634fad27bb5a266c8464302edad
Udaypm123 <udaypm70@gmail.com>
2024-09-03 10:46:43 +0530

    Add files via upload

---
+++ uday/basics.js
@@ -0,0 +1,15 @@
+console.log("Today's Temperature")
+temp = 45
+if (temp < 20)
+{
+    console.log("Cold Day")
+}
+    
+else if(temp>=20 && temp<=30)
+{
+    console.log("Normal Day")
+}
+else
+{
+    console.log("Hot Day") 
+}
